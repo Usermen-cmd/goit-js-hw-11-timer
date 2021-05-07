32e53b2ae8ce7821a44e2de332bfeb0668293d61
Serhii Tomashkevich <19tomat@gmail.com>
2021-05-07 18:40:52 +0300

    +add:export default class CountdownTimer

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
 import './styles.css';
-class CountdownTimer {
-  constructor({ selector, targetDate }) {
-    this.timerRef = document.querySelector(selector);
-    this.valueArrRef = this.timerRef.querySelectorAll('.value');
-    this.targetDate = targetDate;
-  }
-  start() {
-    const timerIntervalId = setInterval(() => {
-      const timeLeft = this.targetDate.getTime() - Date.now();
-      if (timeLeft <= 0) {
-        this.stop(timerIntervalId);
-        return;
-      }
-      this.transformTime(timeLeft);
-    }, 1000);
-  }
 
-  transformTime(time) {
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
-    this.setTimer({ days, hours, mins, secs });
-  }
-  setTimer(obj) {
-    this.valueArrRef.forEach(el => (el.textContent = obj[el.dataset.value]));
-  }
-  stop(id) {
-    clearInterval(id);
-  }
-}
+import CountdownTimer from './timer.js';
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('May 15, 2021 03:23:30'),
+  targetDate: new Date('May 20, 2021 18:00:00'),
 });
 timer.start();
